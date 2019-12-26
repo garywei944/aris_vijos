e7716879725984a62d6af52bbd73352bf53bd4e5
garywei944 <33930674+garywei944@users.noreply.github.com>
2019-12-27 07:54:22 +0800

    Update test.cpp

--- demo/test.cpp
+++ demo/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 
 int main(int argc, char *argv[]) {
@@ -6,6 +7,10 @@ int main(int argc, char *argv[]) {
 #ifdef DEBUG
 	cout << "DEBUG" << endl;
 #endif
+	int i=0;
+	while(1){
+		cout<<"this is forever loop "<<i++<<endl;
+		sleep(1);
+	}
 	return 0;
 }
- 
