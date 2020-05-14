ea291d747a01f4a2dc1026f2ef2ed31f53182d7e
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-05-14 16:12:50 -0400

    Update 1772.c

--- src/1772/1772.c
+++ src/1772/1772.c
@@ -2,10 +2,8 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	_Bool arr[9];
 	char s[12];
 	for (int i = 123; i <= 329; i++) {
-		memset(arr, 0, 9);
 		sprintf(s, "%d %d %d", i, i * 2, i * 3);
 		if (strchr(s, '1') && strchr(s, '2') && strchr(s, '3') && strchr(s, '4') && strchr(s, '5') && strchr(s, '6') && strchr(s, '7') && strchr(s, '8') && strchr(s, '9'))
 			printf("%s\n", s);
